aeed7fffe56df5088237adda11fad01d806b077d
Leavexealone <oyou@ocadu.ca>
2025-10-01 18:20:30 -0400

    cat meme

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+let cowgif;
+
+function preload()
+{
+    cowgif = loadImage('assets/cow.gif');
+}
+
 function setup() 
 {
 	createCanvas(400, 400);
@@ -5,5 +12,6 @@ function setup()
 
 function draw()
 {
-
+    background(220, 200, 200);
+    image(cowgif, 0, 0);
 }
